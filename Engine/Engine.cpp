9247b0fad4967d6836f2c6bd95e4ca9d627c683a
Minseo Han <kuala2038@gmail.com>
2026-08-26 00:18:02 +0900

    5. 장치 초기화: Engine.h, cpp 처리 및 Device 코드

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -2,12 +2,33 @@
 #include "Engine.h"
 
 
-void Engine::Init()
+void Engine::Init(const WindowInfo& info)
 {
+	// 출력 윈도우 핸들/윈도우 관련 설정들
+	_window = info;
+	ResizeWindow(info.width, info.height);
+
+	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
+	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
+
+	_device = make_shared<Device>();
+	_cmdQueue = make_shared<CommandQueue>();
+	_swapChain = make_shared<SwapChain>();
+	_descHeap = make_shared<DescriptorHeap>();
 
 }
 
 void Engine::Render()
 {
 
+}
+
+void Engine::ResizeWindow(int32 width, int32 height)
+{
+	_window.width = width;
+	_window.height = height;
+
+	RECT rect = { 0, 0, width, height };
+	::AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
 }
--- Engine/Engine.h
+++ Engine/Engine.h
@@ -6,8 +6,9 @@ class Engine
 {
 public:
 
-	void Init();
+	void Init(const WindowInfo& info);
 	void Render();
+	void ResizeWindow(int32 width, int32 height);
 
 private:
 	// 그려질 화면 크기 관련
@@ -15,5 +16,10 @@ private:
 	D3D12_VIEWPORT	_viewport = {};
 	D3D12_RECT		_scissorRect = {};
 
+
+	shared_ptr<class Device> _device;
+	shared_ptr<class CommandQueue> _cmdQueue;
+	shared_ptr<class SwapChain> _swapChain;
+	shared_ptr<class DescriptorHeap> _descHeap;
 };
 
