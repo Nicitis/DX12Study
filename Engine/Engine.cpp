540bd15024a16134b4144d4582df3e45980ff019
Minseo Han <kuala2038@gmail.com>
2026-08-26 23:26:35 +0900

    5. 장치 초기화: CommandQueue

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,5 +1,9 @@
 #include "pch.h"
 #include "Engine.h"
+#include "Device.h"
+#include "CommandQueue.h"
+#include "SwapChain.h"
+#include "DescriptorHeap.h"
 
 
 void Engine::Init(const WindowInfo& info)
@@ -16,6 +20,7 @@ void Engine::Init(const WindowInfo& info)
 	_swapChain = make_shared<SwapChain>();
 	_descHeap = make_shared<DescriptorHeap>();
 
+	_device->Init();
 }
 
 void Engine::Render()
